a1ca9a0e19471427ec38990179294e60d54e9b57
kobehuynh <kkobehuynh@gmail.com>
2025-04-17 11:27:52 -0400

    Changed input from list of amino acids to list of chain ids and residue numbers

--- goal1.py
+++ goal1.py
@@ -1,7 +1,8 @@
 """
 Goal 1:
-Module for computing the 3D coordinates of a bounding cuboid for a given PDB file and a set of amino acids.
-This can be computed using only the alpha carbons or all atoms in the specified residues.
+This module computes the 3D bounding cuboid coordinates that encapsulate a specified set of residues
+from a given PDB file. The cuboid is computed using either all atoms or only the alpha carbon (Cα) atoms
+of the specified residues.
  
 This module uses the BioPython package to parse PDB files and compute the bounding cuboid 
 either by only considering alpha carbons or by considering all atoms in specified residues.
@@ -12,14 +13,10 @@ Dependencies include:
 -numpy
 """
 
-#Import packages
-
-from Bio.PDB import *
-parser = PDBParser(PERMISSIVE=1) 
-
+from Bio.PDB import PDBParser
 import numpy as np
 
-def get_coord_cuboid(pdb_file, amino_acids=None, use_alpha_carbon = True):
+def get_coord_cuboid(pdb_file, chain_id, residue_number, use_alpha_carbon = True):
 
     """
     Compute the coordinates of the 3D bounding cuboid for a set of amino acids in a PDB file
@@ -28,24 +25,34 @@ def get_coord_cuboid(pdb_file, amino_acids=None, use_alpha_carbon = True):
     pdb_file: str
         Path to pdb file to be analyzed.
 
-    amino_acids: list[str], optional, default=None  
-        List of amino acid residue names (3-letter codes, e.g, "ALA", "GLY") to be analyzed.
-        If None, all residues are analyzed.
+    chain_id : list of str
+        List of chain identifiers corresponding to the target residues.
+
+    residue_number : list of int
+        List of residue numbers corresponding to the target residues.
+        Must be the same length and order as `chain_id`.
 
     use_alpha_carbon: boolean, default=True
         Whether to use only alpha carbons (Cα) for computing the bounding cuboid.
         If False, all atoms in the residue will be used.
-
+    
     Output:
     Coordinates of the cuboid:
     -Xmax, Ymax, Zmax: float   
     -Xmin, Ymin, Zmin: float
 
     Example:
-    get_coord_cuboid("example.pdb", amino_acids=["ALA", "GLY"], use_alpha_carbon = True)
+    get_coord_cuboid("your_file.pdb", chain_id = ["A","A","A","A","A","B","B","C"], residue_number= [1,2,3,4,5,4,5,7], use_alpha_carbon = True)
     """
 
+    if len(chain_id) != len(residue_number):
+        raise ValueError("'chain_id' list and 'residue_number' list must be of the same length")
+
+    #Create list of tuples using chain ids and residue numbers
+    residue_tuple = list(zip(chain_id, residue_number))
+
     #Load and parse the structure from the PDB file
+    parser = PDBParser(PERMISSIVE=1) 
     structure = parser.get_structure("protein", pdb_file)
 
     #Initialize an empty list to store the 3D atom coordinates
@@ -55,13 +62,14 @@ def get_coord_cuboid(pdb_file, amino_acids=None, use_alpha_carbon = True):
     for model in structure:
         for chain in model: 
             for residue in chain:
-                if amino_acids is None or residue.resname in amino_acids:
-                    for atom in residue:
-                        if use_alpha_carbon == True:
-                            if atom.name == "CA":
-                                coord.append(atom.coord)
-                        else:
-                            coord.append(atom.coord)  
+                hetfield, resseq, icode = residue.get_id()
+                if hetfield == ' ' and (chain.id, resseq) in residue_tuple:
+                    for atom in residue:         
+                        if not use_alpha_carbon or atom.name == "CA":
+                            coord.append(atom.coord)
+            
+    if not coord:
+        raise ValueError("No coordinates were collected. Check your chain/residue input or PDB file.")
 
     #Convert the list of coordinates to a numpy array for easier manipulation
     coord_numpy = np.array(coord)
@@ -70,6 +78,13 @@ def get_coord_cuboid(pdb_file, amino_acids=None, use_alpha_carbon = True):
     x_max,y_max,z_max = np.max(coord_numpy, axis=0) 
     x_min,y_min,z_min = np.min(coord_numpy, axis=0)
 
-    #Print bounding cuboid coordinates 
-    print("Xmax=",x_max,"Ymax=",y_max,"Zmax=",z_max,"Xmin=",x_min,"Ymin=",y_min, "Zmin=",z_min)
-    
+    return {
+        "Xmax": x_max,
+        "Ymax": y_max,
+        "Zmax": z_max,
+        "Xmin": x_min,
+        "Ymin": y_min,
+        "Zmin": z_min
+    }
+
+
--- goal2.py
+++ goal2.py
@@ -1,6 +1,6 @@
 """
 Goal 2: 
-Module for computing the 3D coordinates of a fixed cuboid centered at the mean coordinates of specified amino acids from a PDB file.
+Module for computing the 3D coordinates of a fixed cuboid centered at the mean coordinates of a specified set of residues from a PDB file.
  
 This module uses the BioPython package to parse PDB files and compute the fixed cuboid 
 either by considering only alpha carbons, side chain atoms, or considering all atoms in specified residues.
@@ -11,12 +11,10 @@ Dependencies include:
 -numpy
 """
 
-#Import packages
-
-from Bio.PDB import *  
+from Bio.PDB import PDBParser
 import numpy as np
 
-def get_coord_cuboid_center(pdb_file, amino_acids=None, mean_coord_type = "all_atoms", cuboid_dimensions = []):
+def get_coord_cuboid_center(pdb_file, chain_id, residue_number, mean_coord_type = "all_atoms", cuboid_dimensions = None):
 
     """
     Compute the coordinates of the 3D bounding cuboid for a set of amino acids in a PDB file given a list of dimensions (x,y,z lengths of cuboid
@@ -27,9 +25,12 @@ def get_coord_cuboid_center(pdb_file, amino_acids=None, mean_coord_type = "all_a
     pdb_file: str
         Path to pdb file to be analyzed.
 
-    amino_acids: list[str], optional, default=None  
-        List of amino acid residue names (3-letter codes, e.g, "ALA", "GLY") to be analyzed.
-        If None, all residues are analyzed.
+    chain_id : list of str
+        List of chain identifiers corresponding to the target residues.
+
+    residue_number : list of int
+        List of residue numbers corresponding to the target residues.
+        Must be the same length and order as `chain_id`.
 
     mean_coord_type : str, optional, default="all_atoms"
         The type of atomic coordinates to use for calculating the center of the cuboid. 
@@ -37,10 +38,11 @@ def get_coord_cuboid_center(pdb_file, amino_acids=None, mean_coord_type = "all_a
         - "alpha_carbons": Use only the α-carbon atom coordinates.
         - "all_atoms": Use coordinates from all atoms in the amino acids.
         - "side_chain_atoms": Use coordinates from side-chain atoms only (excludes backbone atoms: CA, O, N, and C).
-        
+
     cuboid_dimensions : list
-        A list of three positive values representing the cuboid's lengths along the x, y, and z axes.
-        If a single value is provided, a cube is assumed with equal lengths for all axes.
+    If one value is provided, a cube with equal side lengths is created.
+    If three values are provided, the cuboid will be of the specified x, y, z lengths.
+
 
     Output:
     Coordinates of the cuboid:
@@ -48,14 +50,20 @@ def get_coord_cuboid_center(pdb_file, amino_acids=None, mean_coord_type = "all_a
     -Xmin, Ymin, Zmin: float
 
     Example:
-    get_coord_cuboid_center("example.pdb", amino_acids=["ALA", "GLY"], mean_coord_type = "side_chain_atoms", cuboid_dimensions = [10,12,14])
+    get_coord_cuboid_center("your_file.pdb", chain_id = ["A","A","A","B","B","C","C","D"], residue_number= [1,2,3,4,5,4,5,7], mean_coord_type = "side_chain_atoms", cuboid_dimensions = [10,12,14])
 
     """
     
+    if len(chain_id) != len(residue_number):
+        raise ValueError("'chain_id' list and 'residue_number' list must be of the same length")
+
+    #Create list of tuples using chain ids and residue numbers
+    residue_tuple = list(zip(chain_id, residue_number))
+
     #Check that the mean coordinate type is valid
     valid_coord_types = ["alpha_carbons","all_atoms","side_chain_atoms"]
     if mean_coord_type not in valid_coord_types:
-        raise ValueError("Invalid coordinate type entered. Please enter one of the following:" "alpha_carbons", "all_atoms", "side_chain_atoms")
+        raise ValueError("Invalid coordinate type entered. Choose from: 'alpha_carbons', 'all_atoms', 'side_chain_atoms'")
 
     #Check that the cuboid dimensions are valid
     if cuboid_dimensions is None:
@@ -79,7 +87,8 @@ def get_coord_cuboid_center(pdb_file, amino_acids=None, mean_coord_type = "all_a
     for model in structure:
         for chain in model: 
             for residue in chain:
-                if amino_acids is None or residue.resname in amino_acids:
+                hetfield, resseq, icode = residue.get_id()
+                if hetfield == ' ' and (chain.id, resseq) in residue_tuple:
                     for atom in residue:
                         if mean_coord_type == "alpha_carbons":
                             if atom.name == "CA":
@@ -89,6 +98,9 @@ def get_coord_cuboid_center(pdb_file, amino_acids=None, mean_coord_type = "all_a
                         elif mean_coord_type == "side_chain_atoms": 
                             if atom.name not in ("CA","O","N","C"):
                                 coord.append(atom.coord)
+
+    if not coord:
+        raise ValueError("No coordinates collected. Check residue selection and mean_coord_type.")
 
     #Convert the list of coordinates to a numpy array for easier manipulation
     coord_numpy = np.array(coord)
@@ -105,5 +117,12 @@ def get_coord_cuboid_center(pdb_file, amino_acids=None, mean_coord_type = "all_a
     z_min = z_mean - cuboid_dimensions[2]/2
 
     #Return fixed cuboid coordinates
-    return("Xmax=",x_max,"Ymax=",y_max,"Zmax=",z_max,"Xmin=",x_min,"Ymin=",y_min, "Zmin=",z_min)
-    
+    return {
+        "Xmax": x_max,
+        "Ymax": y_max,
+        "Zmax": z_max,
+        "Xmin": x_min,
+        "Ymin": y_min,
+        "Zmin": z_min
+    }
+
